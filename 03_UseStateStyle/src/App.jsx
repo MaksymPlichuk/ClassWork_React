8ac0123d718cdf7cce510aa6809865825b3d2c7b
Maksym <pilipm449@gmail.com>
2026-01-11 19:51:31 +0200

    Tasks 1-5 done HW

--- 03_UseStateStyle/src/App.jsx
+++ 03_UseStateStyle/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { MainContent, UserButton } from './components/MainContent'
+import { MainContent, UserButton, RestaurantInfo } from './components/MainContent'
 
 function App() {
+  const [userCount, setUserCount] = useState(0);
+
   return (
     <>
       <MainContent />
-      <UserButton ammount="+90" />
+    
+      <div>{userCount}</div>
+
+      <div>
+        <UserButton ammount="-90" setUserCount={setUserCount} />
+        <UserButton ammount="+100" setUserCount={setUserCount} />
+        <UserButton ammount="+25" setUserCount={setUserCount} />
+      </div>
+
+      <RestaurantInfo FirstName="Puzata Hata" FirsAdress="Khreschatyk St, 15, 4-A, Kyiv, 01001" FirsRating="4.4" FirsKitchenType="Ukranian" FirsImgUrl="https://lh3.googleusercontent.com/gps-cs-s/AG0ilSzEWrIVKWJz20Yh3aVBnvjR1FqhkQGfmafm2yeFeQIKFQSzTQX0Ly4irQQmceiXSJ3DieaasgMs1Y_jfRh4mrXFh-Iam3jFDpi6oF6IJOksxuYxo67bKkKTwpsDjnqrplDX_rA=w325-h218-n-k-no"/>
     </>
   )
 }
--- 03_UseStateStyle/src/components/MainContent.jsx
+++ 03_UseStateStyle/src/components/MainContent.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export const MainContent = () => {
 
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
 
     return (
         <>
@@ -12,33 +13,51 @@ export const MainContent = () => {
             </button>
 
             <h4>Task 2</h4>
-            <div className="counter">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    +1
-                </button>
-                <div className="card" style={{ backgroundColor: "grey" }}>
-                    {count}
+            <div className="counter" style={{ display: "flex", justifyContent: "center" }}>
+                <button onClick={() => setCount2((count2) => count2 + 1)}>+1</button>
+                
+                <div className="card" style={{ backgroundColor: "grey", borderRadius: 5 }}>
+                    {count2}
                 </div>
             </div>
 
             <h4>Task 3</h4>
-
         </>
     );
 }
-export const UserButton = ({ ammount }) => {
 
-    let fullNum = ammount.split("+");
-    if (fullNum.length == 1)  {
-        fullNum = ammount.split("-");
-    }
+export const UserButton = ({ ammount, setUserCount }) => {
 
+    return (
+        <>
+            <button style={{ margin: "5px 5px" }} onClick={() => setUserCount(val => val + parseInt(ammount))}> {ammount} </button>
+        </>
+    );
+}
 
-    let num = parseInt(fullNum[1])
-    const [count, setCount] = useState(0);
+export const RestaurantInfo = ({ FirstName, FirsAdress, FirsRating, FirsKitchenType, FirsImgUrl }) => {
+
+    const [name, setName] = useState(FirstName);
+    const [adress, setAdress] = useState(FirsAdress);
+    const [rating, setRating] = useState(FirsRating);
+    const [kitchenType, setKitchenType] = useState(FirsKitchenType);
+    const [imgUrl, setImgUrl] = useState(FirsImgUrl);
+
     return (
         <div>
-            <button onClick={() => setCount((count) => count + num)}> {ammount} </button>
+            <h4>Task 4-5</h4>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <input style={{ margin: "5px 2px" }} value={name} onChange={(event) => setName(event.target.value)} />
+                    <input style={{ margin: "5px 2px" }} value={adress} onChange={(event) => setAdress(event.target.value)} />
+                    <input style={{ margin: "5px 2px" }} value={rating} onChange={(event) => setRating(event.target.value)} type="number" max={5} min={0} />
+                    <input style={{ margin: "5px 2px" }} value={kitchenType} onChange={(event) => setKitchenType(event.target.value)} />
+                    <input style={{ margin: "5px 2px" }} value={imgUrl} onChange={(event) => setImgUrl(event.target.value)} />
+                </div>
+                <div style={{ margin: "0px 5px" }}>
+                    <img style={{ maxWidth: 250, maxHeight: 250 }} src={imgUrl} alt="imgByUser" />
+                </div>
+            </div>
         </div>
     );
 }
